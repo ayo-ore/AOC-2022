a8ee628b5d17ce2d77c5794869ef7264ea4fe0dd
ayo-ore <42537519+ayo-ore@users.noreply.github.com>
2022-12-01 20:58:47 +1100

    Imported `setup.sh` from last year's repo

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+set -xe
+
+cd `dirname $0`
+typeset -Z2 nextday
+nextday=$((`find * -type d|sort -nr|head -1` +1)) 
+mkdir $nextday
+cd $nextday
+
+touch sample.txt
+touch input.txt
+main=main.jl
+echo '#!/usr/bin/julia
+const INPUT_FILE = ARGS[1]
+const inputs = map(eachline(INPUT_FILE)) do i
+    throw("Not implemented")
+end
+function part_one(inputs)::Int
+    throw("Not implemented")
+end
+function part_two(inputs)::Int
+    throw("Not implemented")
+end
+println("PART ONE: $(part_one(inputs))")
+println("PART TWO: $(part_two(inputs))")
+' > $main
+
+chmod +x $main
