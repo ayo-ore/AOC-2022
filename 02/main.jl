b86e295c1120ed2324d71bb21b0cbd0a22fa7af9
ayo-ore <ayo.ore23@gmail.com>
2022-12-03 12:27:35 +1100

    Using ASCII codes

--- 02/main.jl
+++ 02/main.jl
@@ -1,28 +1,23 @@
 #!/usr/bin/julia
 const INPUT_FILE = ARGS[1]
-const games = readlines(INPUT_FILE)
 
-const OPPONENT_CHARS = collect("ABC")
-const PLAYER_CHARS = collect("XYZ")
-
-get_move(char, charset) = indexin([char], charset)[]
+const games = map(eachline(INPUT_FILE)) do i
+    Int(i[1])-64, Int(i[3])-87
+end
 
 function part_one(games)::Int
-    sum(games) do g
-        opponent_move = get_move(g[1], OPPONENT_CHARS)
+    sum(games) do (opponent_move, player_move)
         outcome = mod(player_move - opponent_move + 1, 3)
         player_move + 3 * outcome
     end
 end
 
 function part_two(games)::Int
-    sum(games) do g
-        outcome = get_move(g[3], PLAYER_CHARS)
-        opponent_move = get_move(g[1], OPPONENT_CHARS)
+    sum(games) do (opponent_move, outcome)
         player_move = mod1(opponent_move + outcome - 2, 3)
         player_move + 3 * outcome - 3
     end
 end
+
 println("PART ONE: $(part_one(games))")
 println("PART TWO: $(part_two(games))")
-
